38147c0c1cf649b082ad70e251c0ca42f7ccfd3d
Falcon Wong <wywfalcon@gmail.com>
2018-08-06 15:19:02 -0700

    remove kv code from overlay

--- cilantro/protocol/overlay/protocol.py
+++ cilantro/protocol/overlay/protocol.py
@@ -25,7 +25,6 @@ class KademliaProtocol(RPCProtocol):
         Get ids to search for to keep old buckets up to date.
         """
         ids = []
-        # TODO re-run discovery here
         for bucket in self.router.getLonelyBuckets():
             rid = random.randint(*bucket.range).to_bytes(20, byteorder='big')
             ids.append(rid)
@@ -36,14 +35,6 @@ class KademliaProtocol(RPCProtocol):
         self.welcomeIfNewNode(source)
         return self.sourceNode.id, self.sourceNode.public_key
 
-    def rpc_store(self, sender, public_key, nodeid, key, value):
-        source = Node(nodeid, sender[0], sender[1], public_key=public_key)
-        self.welcomeIfNewNode(source)
-        log.debug("got a store request from %s, storing '%s'='%s'",
-                  sender, key.hex(), value)
-        self.storage[key] = value
-        return True
-
     def rpc_find_node(self, sender, public_key, nodeid, key):
         log.info("finding neighbors of %i in local table",
                  int(nodeid.hex(), 16))
@@ -53,14 +44,6 @@ class KademliaProtocol(RPCProtocol):
         neighbors = self.router.findNeighbors(node, exclude=source)
         return list(map(tuple, neighbors))
 
-    def rpc_find_value(self, sender, public_key, nodeid, key):
-        source = Node(nodeid, sender[0], sender[1], public_key=public_key)
-        self.welcomeIfNewNode(source)
-        value = self.storage.get(key, None)
-        if value is None:
-            return self.rpc_find_node(sender, public_key, nodeid, key)
-        return {'value': value}
-
     async def callFindNode(self, nodeToAsk, nodeToFind):
         address = (nodeToAsk.ip, nodeToAsk.port)
         result = await self.find_node(address, self.sourceNode.public_key, self.sourceNode.id,
@@ -69,24 +52,9 @@ class KademliaProtocol(RPCProtocol):
             nodeToAsk = None
         return self.handleCallResponse(result, nodeToAsk)
 
-    async def callFindValue(self, nodeToAsk, nodeToFind):
-        address = (nodeToAsk.ip, nodeToAsk.port)
-        result = await self.find_value(address, self.sourceNode.public_key, self.sourceNode.id,
-                                       nodeToFind.id)
-        if not await self.network.authenticate(nodeToAsk):
-            nodeToAsk = None
-        return self.handleCallResponse(result, nodeToAsk)
-
     async def callPing(self, nodeToAsk):
         address = (nodeToAsk.ip, nodeToAsk.port)
         result = await self.ping(address, self.sourceNode.public_key, self.sourceNode.id)
-        if not await self.network.authenticate(nodeToAsk):
-            nodeToAsk = None
-        return self.handleCallResponse(result, nodeToAsk)
-
-    async def callStore(self, nodeToAsk, key, value):
-        address = (nodeToAsk.ip, nodeToAsk.port)
-        result = await self.store(address, self.sourceNode.public_key, self.sourceNode.id, key, value)
         if not await self.network.authenticate(nodeToAsk):
             nodeToAsk = None
         return self.handleCallResponse(result, nodeToAsk)
@@ -122,8 +90,6 @@ class KademliaProtocol(RPCProtocol):
                 newNodeClose = node.distanceTo(keynode) < last
                 first = neighbors[0].distanceTo(keynode)
                 thisNodeClosest = self.sourceNode.distanceTo(keynode) < first
-            if len(neighbors) == 0 or (newNodeClose and thisNodeClosest):
-                asyncio.ensure_future(self.callStore(node, key, value))
 
         self.router.addContact(node)
         self.network.connect_to_neighbor(node)
@@ -134,9 +100,6 @@ class KademliaProtocol(RPCProtocol):
         we get no response, make sure it's removed from the routing table.
         """
         if not result[0]:
-            # if node:
-            #     log.warning("no response from %s, removing from router", node)
-            #     self.router.removeContact(node)
             return result
 
         log.info("got successful response from %s", node)
